a4fb199d8eeb4a79d15e00c216d2bc4249f2e663
vincent verlaan <vincentverlaan33@outlook.fr>
2024-06-09 18:38:03 +0200

    add save mongoose comment

--- microservices/catsWiki.js
+++ microservices/catsWiki.js
@@ -44,5 +44,44 @@ exports.getKittens = (req, res) =>{
     // });
 
 };
+//mongoose.connect('mongodb://user:mysecretpassword@0.0.0.0:8002/cesi_eats_mongo');
+
+// async function main(){
+//     await mongoose.connect('mongodb://user:mysecretpassword@0.0.0.0:8002/')
+//     .then(()=> {
+//         console.log('Connected to mongo');
+//     });
+
+//     const kittySchema = new mongoose.Schema({
+//         name :String,
+//         activity : String,
+//         rarity : String,
+//     });
+
+//     const Cats = mongoose.model('Cats', kittySchema);
+
+//     const MrFresh = new Cats(
+//         { 
+//             name: 'MrFresh', 
+//             activity : 'FoodCritic', 
+//             rarity : 'Common'
+//         }
+//     );
+//     const TheGluttonousBeast = new Cats(
+//         {
+//             name : 'TheGluttonousBeast',
+//             activity:'Eating',
+//             rarity:'Uncommon',
+//         }
+//     );
+
+//     //const AllKitten = [MrFresh, TheGluttonousBeast]
+//     await MrFresh.save();
+//     await TheGluttonousBeast.save();
+
+// }
+
+// const test= main();
+
 
 
